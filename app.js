a038eaf4785971c1564e916e56fac817395c095d
Diya050 <124448340+Diya050@users.noreply.github.com>
2023-02-27 23:20:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function areaPer(){
   var area = w * h;
   var perimeter = w *2 + h*2;
   
-  var result = "Area: " + area + " Perimeter: " + perimeter;
+  var result = "Area: " + area + "   Perimeter: " + perimeter;
   
   document.getElementById("output").innerHTML = result; 
 }
