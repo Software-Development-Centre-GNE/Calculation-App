393c88f3083350bf9b2814caf05e5d804d0c7d4c
geekabhinav007 <abhinav26january@gmail.com>
2023-02-28 00:06:47 +0530

    corrected synatx for calculating perimeter

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-function areaPer(){
+function areaPer() {
   var w = document.getElementById("width").value;
   var h = document.getElementById("height").value;
-  
+
   var area = w * h;
-  var perimeter = w *2 + h*2;
-  
+  var perimeter = 2 * (w + h);
+
   var result = "Area: " + area + "   Perimeter: " + perimeter;
-  
-  document.getElementById("output").innerHTML = result; 
+
+  document.getElementById("output").innerHTML = result;
 }
