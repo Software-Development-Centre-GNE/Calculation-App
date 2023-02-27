4b5d3d8b3bbf8704a9ee28aea03595ebf4f6ea9f
geekabhinav007 <abhinav26january@gmail.com>
2023-02-28 03:33:09 +0530

    able to calculate floating value

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function areaPer() {
-  var w = parseInt(document.getElementById("width").value);
-  var h = parseInt(document.getElementById("height").value);
+  var w = parseFloat(document.getElementById("width").value);
+  var h = parseFloat(document.getElementById("height").value);
   if (w < 0 || h < 0) {
     alert("Height or width cannot be negative!");
     return;
