022648150544cf119dcb1afa17c12993fa11d47c
geekabhinav007 <abhinav26january@gmail.com>
2023-02-28 00:14:21 +0530

    converted string to int for correct calculation

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function areaPer() {
-  var w = document.getElementById("width").value;
-  var h = document.getElementById("height").value;
+  var w = parseInt(document.getElementById("width").value);
+  var h = parseInt(document.getElementById("height").value);
 
   var area = w * h;
   var perimeter = 2 * (w + h);
